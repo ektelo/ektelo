b4c175e99161e027f2f691bf8444e8141ffaacc2
Dan <dzhang@cs.umass.edu>
2018-10-22 16:42:58 -0400

    QuadTree selection for non-power-of-2 domain

--- ektelo/client/selection.py
+++ ektelo/client/selection.py
@@ -466,40 +466,59 @@ class QuadTree(SelectionOperator):
                np.vstack((cur_rect_u[0] + horizontal_offsets, cur_rect_u[1] + vertical_offsets)).T
 
     @staticmethod
+    def same_shape(rect_l, rect_u):
+        '''
+        Check the shape of all candidate rects,
+        return the indices in groups with the same shape
+        '''
+        dic = dict()
+        shape = np.array(rect_u) - np.array(rect_l)
+        for i in range(len(shape)):
+            s = tuple(shape[i])
+            if s in dic:
+                dic[s].append(i)
+            else:
+                dic[s] = [i]
+        return list(dic.values())
+
+    @staticmethod
     def quadtree(n, m):
         ''' Quadtree function, accepts a shape (n, m)
             and returns a set of measurements in sparse format on the expanded x vector
             n and m can be arbitrary numbers
         '''
         # Avoid doing recursion, python is notoriously bad at it
-        assert n != 0 and ((n& (n - 1)) == 0) and m != 0 and ((m & (m - 1)) == 0),\
-        "Current implementation only works for dimensions that is a power of 2"
-        # TODO: deal with non-power-of-2 cases.
-
         pending_l, pending_u = [ (0, 0) ], [(n - 1, m - 1)]
-        pending_offsets =[ ([], []) ]
+
+        pending_offsets_h, pending_offsets_v = [[[0]]], [[[0]]]
         selected_rects_l, selected_rects_u = [], [] 
         while len(pending_l) != 0:
             cur_rect_l = pending_l.pop()
             cur_rect_u = pending_u.pop()
-            offset_h, offset_v = pending_offsets.pop()
+            offset_h = pending_offsets_h.pop()
+            offset_v = pending_offsets_v.pop()
             # resolve offsets in any pending ranges
             lower, higher = QuadTree.expand_offsets(cur_rect_l, cur_rect_u, offset_h, offset_v)
             selected_rects_l.extend(lower)
             selected_rects_u.extend(higher)
             # if it's a leaf then we don't want to split it anymore
             if (cur_rect_u[0] - cur_rect_l[0] + 1) * (cur_rect_u[1] - cur_rect_l[1] + 1)  > 1:
-                    sub_rects_l, sub_rects_u = QuadTree.rect_to_quads((cur_rect_l, cur_rect_u))
+                sub_rects_l, sub_rects_u = QuadTree.rect_to_quads((cur_rect_l, cur_rect_u))
+                same_shape_groups = QuadTree.same_shape(sub_rects_l, sub_rects_u)
+
+                for group_idx in same_shape_groups:
+                    sub_rects_l_group = np.array(sub_rects_l)[group_idx]
+                    sub_rects_u_group = np.array(sub_rects_u)[group_idx]
                     offset_h_level, offset_v_level = [], []
-                    for l,u in sub_rects_l:
-                        offset_h_level.append(l - sub_rects_l[0][0])
-                        offset_v_level.append(u - sub_rects_l[0][1])
-                    offset_h.append(np.array(offset_h_level))
-                    offset_v.append(np.array(offset_v_level))
-                    # append the first quad to pending
-                    pending_l.append(sub_rects_l[0])
-                    pending_u.append(sub_rects_u[0])
-                    pending_offsets.append((offset_h, offset_v))
+                    for l,u in sub_rects_l_group:
+                        offset_h_level.append(l - sub_rects_l_group[0][0])
+                        offset_v_level.append(u - sub_rects_l_group[0][1])
+                    # append the first quad in subgroup to pending list
+                    pending_l.append(sub_rects_l_group[0])
+                    pending_u.append(sub_rects_u_group[0])
+                    pending_offsets_h.append(offset_h + [offset_h_level])
+                    pending_offsets_v.append(offset_v + [offset_v_level])
+
         M = workload.RangeQueries((n,m), np.array(selected_rects_l), np.array(selected_rects_u))
         return M
 
