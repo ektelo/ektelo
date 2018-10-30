c84c0c1df14fa1f340b2aaab8126305e415619ff
Dan <dzhang@cs.umass.edu>
2018-10-30 12:33:10 -0400

    minor fix for H2

--- ektelo/plans/standalone.py
+++ ektelo/plans/standalone.py
@@ -70,13 +70,15 @@ class H2(Base):
     http://dl.acm.org/citation.cfm?id=1920970
     """
 
-    def __init__(self):
+    def __init__(self,domain_shape):
         self.init_params = {}
+        self.domain_shape = domain_shape
         super().__init__()
 
     def Run(self, W, x, eps, seed):
+        x = x.flatten()
         prng = np.random.RandomState(seed)
-        M = selection.H2(x.shape).select()
+        M = selection.H2(self.domain_shape).select()
         y  = measurement.Laplace(M, eps).measure(x, prng)
         x_hat = inference.LeastSquares().infer(M, y)
 
