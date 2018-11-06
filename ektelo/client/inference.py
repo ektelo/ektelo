df59648364518cdb16eef09c29b531f7073e0a51
Dan <dzhang@cs.umass.edu>
2018-11-06 10:30:44 -0500

    remove inference hack

--- ektelo/client/inference.py
+++ ektelo/client/inference.py
@@ -126,8 +126,7 @@ class LeastSquares(InferenceOperator):
 
         if self.method == 'standard':
             assert self.l2_reg == 0, 'l2 reg not supported with method=standard'
-            A = A.dense_matrix() if not isinstance(A, np.ndarray) else A
-            (x_est, _, rank, _) = linalg.lstsq(A, y, lapack_driver='gelsy')
+            (x_est, _, rank, _) = linalg.lstsq(A.dense_matrix(), y, lapack_driver='gelsy')
         elif self.method == 'lsmr':
             res = lsmr(A, y, atol=0, btol=0, damp=self.l2_reg)
             x_est = res[0]
