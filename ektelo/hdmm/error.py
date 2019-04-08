91a1495e83b729c96bd96d397468a186eaa87530
Ryan McKenna <ryan112358@users.noreply.github.com>
2019-04-08 16:32:24 -0400

    hdmm: port over test_error.py

---
+++ ektelo/hdmm/error.py
@@ -0,0 +1,59 @@
+from ektelo import workload
+from ektelo.matrix import EkteloMatrix, VStack
+import numpy as np
+
+def convert_implicit(A):
+    if isinstance(A, EkteloMatrix):
+        return A
+    return EkteloMatrix(A)
+
+def expected_error(W, A, eps=np.sqrt(2), delta=0):
+    """
+    Given a strategy and a privacy budget, compute the expected squared error
+    """
+    assert delta == 0, 'delta must be 0'
+    W, A = convert_implicit(W), convert_implicit(A)
+    AtA1 = A.gram().pinv()
+    WtW = W.gram()
+    # TODO(ryan): fix this hack
+    if isinstance(AtA1, workload.MarginalsGram):
+        X = AtA1 @ WtW
+    else:
+        X = WtW @ AtA1
+    delta = A.sensitivity()
+    trace = X.trace()
+    var = 2.0 / eps**2
+    return var * delta**2 * trace
+
+def rootmse(W, A, eps=np.sqrt(2), delta=0):
+    """ compute a normalized version of expected squared error """
+    return np.sqrt(expected_error(W, A, eps, delta) / W.shape[0])
+
+def squared_error(W, noise):
+    """ 
+    Given a noise vector (x - xhat), compute the squared error on the workload
+    """
+    W = convert_implicit(W)
+    WtW = W.gram()
+    return noisy.dot(WtW.dot(noise))
+
+def average_error_ci(W, noises):
+    """
+    Given a list of noise vectors (x - xhat), compute a 95% confidence interval for the mean squared error.
+    """
+    samples = [squared_error(W, noise) for noise in noises]
+    avg = np.mean(samples)
+    pm = 1.96 * np.std(samples) / np.sqrt(len(samples))
+    return (avg-pm, avg+pm)
+
+def per_query_error(W, A, eps=np.sqrt(2), delta=0, normalize=False):
+    W, A = convert_implicit(W), convert_implicit(A)
+    if isinstance(W, VStack):
+        return np.concatenate([per_query_error(Q, A, eps, delta, normalize) for Q in W.matrices])
+    delta = A.sensitivity()
+    var = 2.0/eps**2
+    AtA1 = A.gram().pinv()
+    X = W @ AtA1 @ W.T
+    err = X.diag()
+    answer = var * delta**2 * err
+    return np.sqrt(answer) if normalize else answer
---
+++ test/hdmm/test_error.py
@@ -0,0 +1,64 @@
+from ektelo import matrix, workload
+from ektelo.hdmm import error
+import numpy as np
+import unittest
+
+class TestError(unittest.TestCase):
+
+    def setUp(self):
+        
+        self.prng = np.random.RandomState(0)
+        
+        self.domain = (2,3,4)
+        I = lambda n: workload.Identity(n)
+        T = lambda n: workload.Total(n)
+        P = lambda n: workload.Prefix(n)
+        R = lambda n: matrix.EkteloMatrix(self.prng.rand(n,n))
+    
+        W1 = workload.Kronecker([I(2), T(3), P(4)])
+        W2 = workload.Kronecker([T(2), T(3), I(4)])
+        W3 = workload.Kronecker([I(2), I(3), T(4)])
+        
+        self.W = workload.VStack([W1, W2, W3])
+
+        # three representations of Identity matrix
+        self.A1 = I(2*3*4)
+        self.A2 = workload.Kronecker([I(2),I(3),I(4)])
+        self.A3 = workload.Marginals.fromtuples(self.domain, {(0,1,2) : 1.0 })
+
+        self.A4 = workload.Marginals(self.domain, self.prng.rand(8))
+        self.A5 = workload.Kronecker([R(2), R(3), R(4)])
+
+    def test_identity(self):
+        ans1 = error.rootmse(self.W, self.A1)
+        ans2 = error.rootmse(self.W, self.A2)
+        ans3 = error.rootmse(self.W, self.A3)
+        print(ans1, ans2, ans3)
+        self.assertTrue(abs(ans1-ans2) <= 1e-10)
+        self.assertTrue(abs(ans1-ans3) <= 1e-10)
+
+        ans1 = error.per_query_error(self.W, self.A1)
+        ans2 = error.per_query_error(self.W, self.A2)
+        ans3 = error.per_query_error(self.W, self.A3)
+
+        np.testing.assert_allclose(ans1, ans2)
+        np.testing.assert_allclose(ans1, ans3)
+
+    def test_per_query(self):
+        # test that total per query error == expected error for all strategies
+        for A in [self.A1, self.A2, self.A3, self.A4, self.A5]:
+            e1 = error.expected_error(self.W, A)
+            e2 = error.per_query_error(self.W, A).sum()
+            print(e1, e2)
+            self.assertTrue(abs(e1-e2) <= 1e-4)
+
+    def test_matrix(self):
+        # test that error using explicit matrices matches error using implicit representations
+        for A in [self.A1, self.A2, self.A3, self.A4, self.A5]:
+            e1 = error.expected_error(self.W, A)
+            e2 = error.expected_error(self.W.dense_matrix(), A.dense_matrix())
+            print(e1, e2)
+            self.assertTrue(abs(e1-e2)/e1 <= 1e-7)
+ 
+if __name__ == '__main__':
+    unittest.main()
